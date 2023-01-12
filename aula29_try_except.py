0aa2ac1a718efdc9c64f9f5a200fea8369328967
Gustavo <gustavo.om1206@gmail.com>
2023-01-12 17:41:48 -0300

    modified:   aula29_try_except.py

--- aula29_try_except.py
+++ aula29_try_except.py
@@ -3,3 +3,11 @@ Introdução ao try/except
 try -> tentar executar o código
 except -> ocorreu algum erro ao tentar executar
 '''
+
+numero_str = input('Vou dobrar o número que você digitar: ')
+
+try:
+    numero_float = float(numero_str)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.1f}')
+except:
+    print('Isso não é um número')
