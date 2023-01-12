b2006dc0bac3fa351d014d883fbc63ddbf16a722
Gustavo <gustavo.om1206@gmail.com>
2023-01-12 19:05:29 -0300

    deleted:    aula31.py 	new file:   aula31_is_e_is_not.py

---
+++ aula31_is_e_is_not.py
@@ -0,0 +1,14 @@
+'''
+Flag (Bandeira) - Marcar um local
+None = não valor
+is e is not = é ou não é (tipo, valor, unidade)
+id = identidade
+'''
+condicao = False
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
