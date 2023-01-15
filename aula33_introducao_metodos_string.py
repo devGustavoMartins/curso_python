33bf62449d03e66f2260ca9e0b5998844744b5c9
Gustavo <gustavo.om1206@gmail.com>
2023-01-15 16:04:16 -0300

    deleted:    aula33.py 	new file:   aula33_introducao_metodos_string.py

---
+++ aula33_introducao_metodos_string.py
@@ -0,0 +1,7 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'gustavo martins'
+print(string.capitalize()) # Primeira letra da frase maiúscula
+print(string.title()) # Primeira letra de cada palavra maiúscula
