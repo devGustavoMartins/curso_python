cc7f0840d771923325b5e2b21031726526d8afdf
Gustavo <gustavo.om1206@gmail.com>
2023-01-15 16:40:26 -0300

    modified:   aula36_atribuicao.py

--- aula36_atribuicao.py
+++ aula36_atribuicao.py
@@ -2,3 +2,17 @@
 Operadores de atribuição
 = += -= *= /= //= **= %=
 """
+contador = 0
+num_exemplo = 2
+
+while contador < 10:
+    contador += 1
+    print(contador)
+
+print()
+print('-------------')
+print()
+
+
+num_exemplo **= 10
+print(num_exemplo)
