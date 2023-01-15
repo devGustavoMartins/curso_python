e25cbf69cb55d812cbdf034f8486e99e146d2284
Gustavo <gustavo.om1206@gmail.com>
2023-01-15 16:34:14 -0300

    modified:   aula35_while_2.py

--- aula35_while_2.py
+++ aula35_while_2.py
@@ -4,8 +4,10 @@ while (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
 Loop infinito -> Quando um código não tem fim
 """
+contador = 0
 
-while True:
-    ...
+while contador < 100:
+    contador = contador + 1
+    print(contador)
 
-print('Acabou')
+print('Terminou de contar!')
