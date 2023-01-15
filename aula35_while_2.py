303787fe93793379ffe020d5570f9f1b6f603eef
Gustavo <gustavo.om1206@gmail.com>
2023-01-15 16:17:06 -0300

    renamed:    aula34_while_e_break.py -> aula34_while_1.py 	deleted:    aula35.py 	new file:   aula35_while_2.py

---
+++ aula35_while_2.py
@@ -0,0 +1,11 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+
+while True:
+    ...
+
+print('Acabou')
