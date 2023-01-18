dead90bf0bc6062003a8337eef4ba55548e4fb2a
Gustavo <gustavo.om1206@gmail.com>
2023-01-18 20:03:18 -0300

    new file:   aula48_list_1.py

---
+++ aula48_list_1.py
@@ -0,0 +1,13 @@
+'''
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+'''
+#        +01234
+#        -54321
+string = 'ABCDE'  # 5 caracteres (len)
+lista = [123, True, 'Gustavo Martins', 1.5]
+print(lista)
+print(lista[2])
