05f2e291a9f54969cb1cede1e8b1155896398942
Gustavo <gustavo.om1206@gmail.com>
2023-01-18 22:34:41 -0300

    new file:   aula54_list_7.py

---
+++ aula54_list_7.py
@@ -0,0 +1,11 @@
+'''
+Exercício -> Exiba os índices da lista
+'''
+lista = ['Gustavo', 'Sophia', 'Enrico']
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice])
+
+# for nome in lista:
+#     print(lista.index(nome), nome)
