3b3ba20e92c8b4802344ba4c556accaa062f863b
Gustavo <gustavo.om1206@gmail.com>
2023-01-19 18:06:59 -0300

    new file:   aula56_list_9.py

---
+++ aula56_list_9.py
@@ -0,0 +1,7 @@
+'''
+Tipo tupla = Uma lista imutável
+'''
+nomes = 'Gustavo', 'Sophia', 'Enrico'
+# nomes[2] = 'Pedro'  # A alteração não funciona em uma tuple (lista sem colchete)
+# qualquer alteração irá gerar um erro no terminal
+print(nomes[0])
