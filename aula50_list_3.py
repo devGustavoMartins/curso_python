0613bb0b2a7f5c59243918dfa3525d1a0d9b3e7e
Gustavo <gustavo.om1206@gmail.com>
2023-01-18 21:57:28 -0300

    new file:   aula50_list_3.py

---
+++ aula50_list_3.py
@@ -0,0 +1,24 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3
+lista = [10, 20, 30, 40]
+print(lista)
+lista.insert(0, 5)
+lista.insert(2, 15)
+lista.insert(4, 25)
+lista.insert(6, 35)
+print(lista)
