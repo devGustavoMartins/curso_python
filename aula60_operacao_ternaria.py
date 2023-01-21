586a0fd5fe502133f3644a3dff30c49bb53b28ce
Gustavo <gustavo.om1206@gmail.com>
2023-01-21 11:34:25 -0300

    new file:   aula60_operacao_ternaria.py

---
+++ aula60_operacao_ternaria.py
@@ -0,0 +1,10 @@
+'''
+Operação ternária (condicional de uma linha)
+<valor> if <condicao> else <outro valor>
+'''
+# condicao = 10 == 9
+# variavel = 'Valor' if condicao else 'Outro valor'
+# print(variavel)
+digito = 9
+novo_digito = digito if digito <= 9 else 0
+print(novo_digito)
