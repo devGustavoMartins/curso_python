ab0c702448f91afe8dbb7b1b89e26011788da2c8
Gustavo <gustavo.om1206@gmail.com>
2023-01-23 18:37:29 -0300

    modified:   aula66_funcao_2.py

--- aula66_funcao_2.py
+++ aula66_funcao_2.py
@@ -7,3 +7,6 @@ def soma(x, y):
     print(x + y)
 
 soma(1, 2)
+soma(x=1, y=2)
+soma(1, y=2)  # É possível utilizar argumentos posicionais antes de argumentos nomeados
+# soma(x=1, 2)  Não pode usar argumentos posicionais após argumentos nomeados
