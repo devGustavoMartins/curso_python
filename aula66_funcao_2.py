578e773345f6c480ba3338d04f6fbaa6d1344b7f
Gustavo <gustavo.om1206@gmail.com>
2023-01-22 21:40:29 -0300

    new file:   aula66_funcao_2.py

---
+++ aula66_funcao_2.py
@@ -0,0 +1,9 @@
+"""
+Argumentos nomeados e não nomeados em funções Python
+Argumento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor)
+"""
+def soma(x, y):
+    print(x + y)
+
+soma(1, 2)
