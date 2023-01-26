69b0533a741c8247b6d8c2479a35aff42174392d
Gustavo <gustavo.om1206@gmail.com>
2023-01-25 21:16:36 -0300

    new file:   aula70_return.py

---
+++ aula70_return.py
@@ -0,0 +1,11 @@
+'''
+Retorno de valores das funções (return)
+'''
+def soma(x, y):
+    if x+y > 10:
+        return 10
+    return x + y
+
+soma1 = soma(1, 10)
+soma2 = soma(3, 8)
+print(soma1 + soma2)
