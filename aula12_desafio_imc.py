7dbc2d3b479ff12b69823f5a4979b50a5e582173
Gustavo <gustavo.om1206@gmail.com>
2023-01-26 21:25:10 -0300

    modified:   aula12_desafio_imc.py

--- aula12_desafio_imc.py
+++ aula12_desafio_imc.py
@@ -1,7 +1,7 @@
 # DESAFIO NA AULA SOBRE CALCULAR IMC
 nome = 'Gustavo Martins'
 altura = 1.8
-peso = 60
+peso = 55
 imc = peso / (altura * altura)
 
 print(nome, 'tem ', altura, 'm de altura, pesa ', peso, 'quilos e seu IMC é ', imc)
